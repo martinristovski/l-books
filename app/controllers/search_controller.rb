4e227f867f361e592a6f22bc855c2b3df60b41d8
Vishnu Nair <vishnu.nair@columbia.edu>
2021-11-01 20:23:51 -0400

    Implementing search form.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,19 @@
+require 'ostruct'
+
 class SearchController < ApplicationController
   def index
+    @search_types = form_search_types
+  end
 
+  def search_from_home
+
+  end
+
+  private
+  def form_search_types
+    c1 = OpenStruct.new(:display_name => "Title/Author", :id => "titleauthor")
+    c2 = OpenStruct.new(:display_name => "Course Number", :id => "course")
+    c3 = OpenStruct.new(:display_name => "ISBN", :id => "isbn")
+    [c1, c2, c3]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'search#index'
+
+  # search
+  post 'search', to: 'search#search_from_home'
 end
