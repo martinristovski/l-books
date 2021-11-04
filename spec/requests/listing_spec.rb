a34182669f4d8ba1b1132fa5b5931818ace61bc8
Vishnu Nair <vishnu.nair@columbia.edu>
2021-11-03 23:34:59 -0400

    Finishing listing spec. 100% coverage.

---
+++ spec/requests/listing_spec.rb
@@ -0,0 +1,141 @@
+require 'rails_helper'
+
+RSpec.describe "Books", type: :request do
+  before(:all) do
+    ## COPIED FROM seeds.rb ##
+    # users
+    u1 = User.create!(
+      first_name: "Vishnu",
+      last_name: "Nair",
+      uni: "vn1234",
+      email: "vn@columbia.edu",
+      school: "SEAS"
+    )
+    u2 = User.create!(
+      first_name: "Ivy",
+      last_name: "Cao",
+      uni: "ic1234",
+      email: "ic@columbia.edu",
+      school: "SEAS"
+    )
+    u3 = User.create!(
+      first_name: "Aditya",
+      last_name: "Satnalika",
+      uni: "as1234",
+      email: "as@columbia.edu",
+      school: "SEAS"
+    )
+    u4 = User.create!(
+      first_name: "Martin",
+      last_name: "Ristovski",
+      uni: "mr1234",
+      email: "mr@columbia.edu",
+      school: "CC"
+    )
+
+    # books
+    b1 = Book.create!(
+      title: "The Iliad of Homer",
+      authors: "Richmond Lattimore; Homer",
+      edition: nil,
+      publisher: "University of Chicago Press",
+      isbn: "9780226470498",
+      image_url: "https://images-na.ssl-images-amazon.com/images/I/61xbfNmcFwL.jpg" # TODO: S3 later.
+    )
+    b2 = Book.create!(
+      title: "Plato Symposium (Hackett Classics)",
+      authors: "Plato; Alexander Nehamas; Paul Woodruff",
+      edition: "1989 Edition",
+      publisher: "Hackett Publishing Co",
+      isbn: "9780872200760",
+      image_url: "https://images-na.ssl-images-amazon.com/images/I/41vx+Jrc8GL.jpg" # TODO: S3 later.
+    )
+    b3 = Book.create!(
+      title: "The Aeneid of Virgil (Bantam Classics)",
+      authors: "Virgil; Allen Mandelbaum",
+      edition: "Revised ed.",
+      publisher: "Bantam Classics",
+      isbn: "9780553210415",
+      image_url: "https://images-na.ssl-images-amazon.com/images/I/71Rd1htsJvL.jpg" # TODO: S3 later.
+    )
+
+    # courses
+    c1 = Course.create!(
+      code: "HUMA1001",
+      name: "Masterpieces of Western Literature and Philosophy I"
+    )
+    c2 = Course.create!(
+      code: "HUMA1002",
+      name: "Masterpieces of Western Literature and Philosophy II"
+    )
+
+    # book-course associations
+    bca1 = BookCourseAssociation.create!(
+      book_id: b1.id,
+      course_id: c1.id
+    )
+    bca2 = BookCourseAssociation.create!(
+      book_id: b2.id,
+      course_id: c1.id
+    )
+    bca3 = BookCourseAssociation.create!(
+      book_id: b3.id,
+      course_id: c1.id
+    )
+
+    # listings
+    l1 = Listing.create!(
+      book_id: b1.id,
+      price: 5.00,
+      condition: "Like new",
+      description: "Copy of the Iliad. Looks like it was never used (which may or may not have been the case)...",
+      seller_id: u1.id
+    )
+    l2 = Listing.create!(
+      book_id: b1.id,
+      price: 4.50,
+      condition: "Used, slightly worn",
+      description: "Another copy of the Iliad. This actually looks like it was used.",
+      seller_id: u2.id
+    )
+    l3 = Listing.create!(
+      book_id: b2.id,
+      price: 5.15,
+      condition: "Used",
+      description: "Plato's Symposium. Used.",
+      seller_id: u3.id
+    )
+  end
+
+  describe "GET /listing/1" do
+    it "renders listing template for an existing listing" do
+      get '/listing/1'
+      expect(response).to render_template('show')
+      expect(assigns(:listing).condition).to eq("Like new")
+    end
+  end
+
+  describe "GET /listing/0" do
+    it "redirect home for a non-existing listing" do
+      get '/listing/0'
+      expect(response).to redirect_to('/')
+      expect(flash[:notice]).to eq("Sorry, we couldn't find a listing with that ID.")
+    end
+  end
+
+  describe "GET /listing/dekoewd" do
+    it "redirect home if non-numeric id given" do
+      get '/listing/dekoewd'
+      expect(response).to redirect_to('/')
+      expect(flash[:notice]).to eq("Sorry, we couldn't find a listing with that ID.")
+    end
+  end
+
+  describe "GET /listing/" do
+    it "redirect home if no id given" do
+      get '/listing/'
+      expect(response).to redirect_to('/')
+    end
+  end
+
+end
