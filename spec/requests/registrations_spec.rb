0f4e690bd5e71da04ff42f84c228f8ae02badc34
martinristovski <martin.ristovski@columbia.edu>
2021-11-17 11:48:15 -0500

    add rspec tests for registrations_controller.rb

---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe "Registrations", type: :request do
+  describe "GET /signup" do
+    it "gets the signup page" do
+      get '/signup'
+      expect(response).to render_template 'new'
+    end
+  end
+
+  describe "Create new user" do
+    it "registers a new user" do
+      params = {}
+      params[:user] = {}
+      params[:user][:email] = "test@test.edu"
+      params[:user][:first_name] = "Test"
+      params[:user][:last_name] = "Test"
+      params[:user][:uni] = "tt1111"
+      params[:user][:school] = "CC"
+      params[:user][:password] = "passtest"
+      params[:user][:password_confirmation] = "passtest"
+      post '/signup', params: params
+      
+      expect(response).to redirect_to '/'
+    end
+  end
+
+  describe "Receive bad input" do
+    it "returns new" do
+      params = {}
+      params[:user] = {}
+      params[:user][:email] = "test@test.edu"
+      params[:user][:first_name] = "Test"
+      params[:user][:last_name] = "Test"
+      params[:user][:uni] = "tt1111"
+      params[:user][:school] = "CC"
+      params[:user][:password] = "passtest"
+      params[:user][:password_confirmation] = "wrongtext"
+      post '/signup', params: params
+      
+      expect(response.response_code).to eq 200 
+    end
+  end
+
+end
