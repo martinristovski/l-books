6d0ef22bf3cf351380f1727b000485b42d3556b6
Vishnu Nair <vishnu.nair@columbia.edu>
2021-11-30 01:50:59 -0500

    Redirect back to previous page on login and logout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
 class SessionsController < ApplicationController
     layout 'home'
-    def new; end
+    def new
+        @fdata = {
+          :redirect_url => request.referrer
+        }
+        puts @fdata
+    end
 
     def create
         # finds existing user, checks to see if user can be authenticated
         user = User.find_by(email: params[:email])
+        puts params[:redirect_url]
 
         if user.nil?
             flash[:notice] = 'Invalid email or password'
@@ -14,7 +20,11 @@ class SessionsController < ApplicationController
 
         if user.present? && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to root_path, notice: 'Logged in successfully'
+            if params[:redirect_url].nil?
+                redirect_to root_path, notice: 'Logged in successfully'
+            else
+                redirect_to params[:redirect_url], notice: 'Logged in successfully'
+            end
         else
             flash[:notice] = 'Invalid email or password'
             render :new
@@ -22,7 +32,16 @@ class SessionsController < ApplicationController
     end
 
     def destroy
+        if session[:user_id].nil?
+            redirect_to root_path, notice: 'Cannot log out if you never logged in.'
+            return
+        end
+
         session[:user_id] = nil
-        redirect_to root_path, notice: 'Logged Out'
+        if request.referrer.nil?
+            redirect_to root_path, notice: 'Logged Out'
+        else
+            redirect_to request.referrer, notice: 'Logged Out'
+        end
     end
 end
