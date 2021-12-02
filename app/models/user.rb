39758945f279e418aca2c0ca2da8ce151181e0e2
martinristovski <martin.ristovski@columbia.edu>
2021-12-02 13:32:32 -0500

    add columbia email registration requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
 
   # validate that the email is in the correct format
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: /\A(.+)@columbia\.edu\z/i , message: "Please use your Columbia email address."}
 
   has_many :seller_listings, class_name: "Listing", foreign_key: "seller_id"
   has_many :user_reputation_ratings
