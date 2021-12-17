951803ec540db5a4d36bb789f1fe866eeb99e88e
Vishnu Nair <vishnu.nair@columbia.edu>
2021-12-16 19:55:46 -0500

    Showing reputation ratings on listing page.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,22 @@ class User < ApplicationRecord
   validates :email, format: { with: /\A(.+)@columbia\.edu\z/i , message: "Please use your Columbia email address."}
 
   has_many :seller_listings, class_name: "Listing", foreign_key: "seller_id"
-  has_many :user_reputation_ratings
+  has_many :ratings_as_seller, class_name: "UserReputationRating", foreign_key: "target_user_id"
+  
+  def get_num_ratings
+    ratings_as_seller.count
+  end
+  
+  def get_avg_rating
+    if ratings_as_seller.count.zero?
+      0.0
+    end
+    
+    sum = 0
+    ratings_as_seller.each do |r|
+      sum += r.score
+    end
+
+    sum / ratings_as_seller.count.to_f
+  end
 end
