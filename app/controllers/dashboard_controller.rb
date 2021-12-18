2d72aef20b04f9d392cf112b05a2cafa33a464f0
Vishnu Nair <vishnu.nair@columbia.edu>
2021-12-17 21:57:39 -0500

    Adding miscellaneous files for dashboard.

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,19 @@
+class DashboardController < ApplicationController
+  # layout 'other_pages'
+
+  def show
+    if session[:user_id].nil?
+      redirect_to '/signin'
+      return
+    end
+
+    # find the user in question
+    @user = User.find_by(id: session[:user_id])
+
+    if @user.nil?
+      flash[:notice] = "Sorry, we couldn't find the user you're logged in as."
+      redirect_to controller: "search", action: 'index'
+      return
+    end
+  end
+end
