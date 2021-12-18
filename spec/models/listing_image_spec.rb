02d87027c0833a8dd87d8ceeeb1c3c5d898a3fe3
Vishnu Nair <vishnu.nair@columbia.edu>
2021-12-18 18:03:53 -0500

    Typo.

--- spec/models/listing_image_spec.rb
+++ spec/models/listing_image_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ListingImage, type: :model do
-  describe "Get a rdndom UUID" do
+  describe "Get a random UUID as the image name" do
     it "returns a properly-formatted UUID" do
       generated = ListingImage.generate_unique_name
       expect(generated.length).to be(36)
-      expect(generated.count('-')).to be(4)
+      expect(generated.count('-')).to be(4) # expect 4 dashes
     end
   end
 end
