ed82a58a9c7ebf2ce855cc98ca794b9380cf5da3
Vishnu Nair <vishnu.nair@columbia.edu>
2021-12-18 18:02:06 -0500

    Adding a small test for listing_image.rb.

--- spec/models/listing_image_spec.rb
+++ spec/models/listing_image_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ListingImage, type: :model do
+  describe "Get a rdndom UUID" do
+    it "returns a properly-formatted UUID" do
+      generated = ListingImage.generate_unique_name
+      expect(generated.length).to be(36)
+      expect(generated.count('-')).to be(4)
+    end
+  end
 end
